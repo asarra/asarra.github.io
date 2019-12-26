576c090b74aeaaf60fc8fd92794c453a3c1cec60
asarra <20255127+asarra@users.noreply.github.com>
2019-12-26 16:40:13 +0100

    Recreated old website

---
+++ custom.js
@@ -0,0 +1,31 @@
+var dialogue=document.getElementById("greeting")
+dialogue.style.cssText="position:relative;font-size:2.5em";
+setTimeout(function(){
+    dialogue.classList.remove('fa-hand-peace');    
+    dialogue.classList.add("fa-eye");
+    dialogue.innerHTML=" I am glad to see you!";},2000);
+setTimeout(function(){
+    dialogue.classList.remove('fa-eye');
+    dialogue.classList.add('fa-smile-beam');
+    dialogue.innerHTML=" I am Mehmet-Ali!";},4000);
+const scale_factor=10, prefix=3;
+document.onkeydown = function _() {
+    e = event.keyCode || window.event;
+    switch(e){
+        case 32:
+            window.scrollBy({top: prefix*10*scale_factor,left: 0});
+            break;
+        case 38:
+            window.scrollBy({top: -1*prefix*scale_factor,left: 0});
+            break;
+        case 40:
+            window.scrollBy({top: prefix*scale_factor,left: 0});
+            break;
+        case 37:
+            window.scrollBy({top: 0,left: -1*prefix*scale_factor});
+            break;
+        case 39:
+            window.scrollBy({top: 0,left: prefix*scale_factor});
+            break;
+    }
+}
