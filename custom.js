dbd11e9d1de8a33644a8abb2cd647ad7c8e9de81
asarra <20255127+asarra@users.noreply.github.com>
2019-12-31 02:06:16 +0100

    + progress scroll bar

--- custom.js
+++ custom.js
@@ -76,3 +76,9 @@ menu.addEventListener('click', function(event) {
     menu.classList.toggle('is-active');
 });
 
+window.onscroll = function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
