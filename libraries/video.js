75d90bf97c9acc30079f950edd46675169895114
Mehmet-Ali <mehmet-ali.asar@protonmail.com>
2021-07-08 17:10:16 +0200

    Update video.js

--- libraries/video.js
+++ libraries/video.js
@@ -2,6 +2,9 @@ var vid = document.getElementById("skills_video");
 var interval = setInterval(checkTime, 100);
 var skillset = document.getElementById("skillset");
 
+//If the video ends, we reset the position to 0
+vid.addEventListener('ended',(e)=>{vid.currentTime = 0},false);
+
 //I disable it in JS instead of in HTML/CSS to ensure that skillset will be shown even if JS is deactivated
 skillset.style.display = "none";
 
