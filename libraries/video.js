da3c068beef213ca13bec9c31e3edd2ee7661308
Mehmet-Ali <mehmet-ali.asar@protonmail.com>
2021-07-09 06:40:56 +0200

    Implemented YT player

--- libraries/video.js
+++ libraries/video.js
@@ -1,13 +1,37 @@
-var vid = document.getElementById("skills_video");
 var interval = setInterval(checkTime, 100);
 var skillset = document.getElementById("skillset");
 
-//If the video ends, we reset the position to 0
-vid.addEventListener('ended',(e)=>{vid.currentTime = 0},false);
+
+
+// modified ytplayer code: https://developers.google.com/youtube/player_parameters#IFrame_Player_API
+var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  var player;
+  function onYouTubePlayerAPIReady() {
+      player = new YT.Player('skills_video', {
+          height: '1080',
+          width: '1920',
+          videoId: 'aPrGES-PcQ4',
+          events: {
+            //onReady: onPlayerReady,
+            onStateChange: onPlayerStateChange
+          }
+      });}
+
+    //If the video ends, we reset the position to 0
+    function onPlayerStateChange(event) {
+        if(event.data === 0) {
+            player.seekTo(0);
+            player.pauseVideo();
+    }
+  }
+
 
 //I disable it in JS instead of in HTML/CSS to ensure that skillset will be shown even if JS is deactivated
 skillset.style.display = "none";
 
 function checkTime() {
-    (vid.currentTime>=24 & vid.currentTime <= 32) ? skillset.style.display = "inline-block" : skillset.style.display = "none";
+        (player.playerInfo.currentTime>=24 & player.playerInfo.currentTime <= 32) ? skillset.style.display = "inline-block" : skillset.style.display = "none";
 }
